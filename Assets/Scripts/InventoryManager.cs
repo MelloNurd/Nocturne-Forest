7b9c75e035c37f45535204820b1806aa37940501
Davin Lewis <djlewis2@nic.edu>
2024-07-18 19:08:48 -0700

    finished inventory system, just needs implemented

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public int maxStackedItems = 4;
+    public int numSlots = 7;
+    public InventorySlot[] inventorySlots;
+    public GameObject inventoryItemPrefab;
+
+    int selectedSlot = -1;
+
+    private void Start()
+    {
+        ChangeSelectedSlot(0);
+    }
+
+    private void Update()
+    {
+        if (Input.inputString != null)
+        {
+            bool isNumber = int.TryParse(Input.inputString, out int number);
+            if(isNumber && number > 0 && number <= numSlots)
+            {
+                ChangeSelectedSlot(number - 1);
+            }
+        }
+    }
+
+    void ChangeSelectedSlot(int newValue)
+    {
+        if (selectedSlot >= 0)
+        {
+            inventorySlots[selectedSlot].Deselect();
+        }
+
+
+        inventorySlots[newValue].Select();
+        selectedSlot = newValue;
+    }
+
+   public bool AddItem(Item item)
+    {
+        //check if slot has the same item with lower count than max
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            inventoryItem itemInSlot = slot.GetComponentInChildren<inventoryItem>();
+            if (itemInSlot != null && itemInSlot.item == item && itemInSlot.count < maxStackedItems && itemInSlot.item.stackable == true)
+            {
+                itemInSlot.count++;
+                itemInSlot.RefreshCount();
+                return true;
+            }
+        }
+
+        //find empty slot
+        for ( int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            inventoryItem itemInSlot = slot.GetComponentInChildren<inventoryItem>();
+            if (itemInSlot == null) 
+            { 
+                SpawnNewItem(item, slot);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void SpawnNewItem(Item item, InventorySlot slot)
+    {
+        GameObject newItemGO = Instantiate(inventoryItemPrefab, slot.transform);
+        inventoryItem inventoryItem = newItemGO.GetComponent<inventoryItem>();
+        inventoryItem.InitializeItem(item);
+    }
+
+    public Item GetSelectedItem(bool use)
+    {
+        InventorySlot slot = inventorySlots[selectedSlot];
+        inventoryItem itemInSlot = slot.GetComponentInChildren<inventoryItem>();
+        if (itemInSlot != null)
+        {
+            Item item = itemInSlot.item;
+            if (use == true)
+            {
+                itemInSlot.count--;
+                if(itemInSlot.count <= 0)
+                {
+                    Destroy(itemInSlot.gameObject);
+                }
+                else
+                {
+                    itemInSlot.RefreshCount();
+                }
+            }
+            return itemInSlot.item;
+        }
+
+        return null;
+    }
+}
