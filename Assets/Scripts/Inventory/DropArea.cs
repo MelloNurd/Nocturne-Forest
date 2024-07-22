8a088e48827f8131ff42c6b53d5e1d0fb4f106b2
Davin Lewis <djlewis2@nic.edu>
2024-07-22 15:48:49 -0700

    ui changes

--- Assets/Scripts/Inventory/DropArea.cs
+++ Assets/Scripts/Inventory/DropArea.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class DropArea : MonoBehaviour, IDropHandler
 {
     InventoryManager inventoryManager;
     public bool trash;
+    [SerializeField] Button button;
 
     private void Start() {
         inventoryManager = InventoryManager.currentInstance;
@@ -23,3 +28,4 @@ public class DropArea : MonoBehaviour, IDropHandler
         Destroy(drugItem.gameObject);
     }
 }
+
